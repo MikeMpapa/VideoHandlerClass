80aa5036cc7f27e09ef0b6b6c9efb29882f94314
Michalis Papakostas <MikeMpapa@users.noreply.github.com>
2019-09-08 13:33:01 -0400

    initialize-repo
    1. main - loads videos from dir as VideoHandlerObjects
    2. VideoHandler - CURRENT SUPPORTS:  +SetCapture() --> file or live capture, +getVideoFrames() --> return all frames in rgb, gray or both as lists, +playbackVideo --> playback a VideoHandler object

---
+++ VideoHandler.py
@@ -0,0 +1,59 @@
+import numpy
+import cv2
+
+class VideoHandler:
+
+    def __init__(self):
+        self.video = ''
+
+    def SetCapture(self,video_input):
+        self.video = cv2.VideoCapture(video_input)
+        return 0
+
+
+    def getVideoFrames(self,flag='all'):
+        '''
+        :param flag: Which set of frames to return - 0:All (Default), 1:RGB, 2:Grayscale
+        :return: list of lists cointaing the sequence of RGB frames --> frames[0] and Gray frames-->frames[1]==frames[-1]
+        '''
+        frames = []
+        if  flag=='all':frames = [[],[]]
+        count = 0
+        while (self.video.isOpened()):
+            ret, frame = self.video.read()
+            count+=1
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            if flag=='all' or flag=='gray':
+                frames[-1].append(gray)
+            if flag == 'all' or flag == 'rgb':
+                frames[0].append(frame)
+            if count==10:
+                break
+        self.video.release()
+        return frames
+
+
+    def playbackVideo(self,):
+        '''
+        playback a VideoHandler Object
+        :return:
+        '''
+        while (self.video.isOpened()):
+            ret, frame = self.video.read()
+
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            cv2.imshow('frame', gray)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        self.release()
+        cv2.destroyAllWindows()
+        return 0
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,37 @@
+from VideoHandler import VideoHandler
+from glob import glob
+
+'''
+vid = VideoHandler()
+
+vid.SetCapture("/home/michalis/Documents/TRI_Stress/sample_data/N14_VideoMonologue1_closeup.avi")
+vid.playbackVideo()
+
+frames = vid.getVideoFrames(0)
+
+print(frames)
+'''
+
+
+def loadVideosFromDir(path_to_data):
+    dirs = glob(path_to_data)
+    return dirs
+
+
+
+if __name__ == "__main__":
+    sample_paths = loadVideosFromDir("/home/michalis/Documents/TRI_Stress/sample_data/*avi")
+    print (sample_paths)
+
+    data_gray = []
+    data_rgb = []
+    for path in sample_paths:
+        vid = VideoHandler()
+        vid.SetCapture(path)
+        frames = vid.getVideoFrames('all')
+        data_gray.append(frames[-1])
+        data_rgb.append(frames[0])
+
+
+    pass
+
